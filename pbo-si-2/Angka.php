1bcc9943bd98a244e9945b25297dbddeeeffe2c3
robbyakakom <robby.akakom@gmail.com>
2022-07-11 22:03:08 +0700

    Update Angka.php
    coba branch

--- pbo-si-2/Angka.php
+++ pbo-si-2/Angka.php
@@ -4,10 +4,12 @@ class Angka
   private $a ;
   public function isiData($isi)
   {
-    $this->a = $isi ;
+    // isi properti a
+    $this->a = $isi ; 
   }
   public function tampilData()
   {
+    // tampilkan properti a
     return $this->a ;
   }
 }
